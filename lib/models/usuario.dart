c02e855993b8d33395006bf30896ed14ada6e983
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-02 16:39:38 -0500

    Update usuario.dart

--- lib/models/usuario.dart
+++ lib/models/usuario.dart
@@ -1,4 +1,14 @@
-class usuario {
+import 'dart:ffi';
+
+class Usuario {
+  String id;
   String nombre;
+  Int edad;
+
+  Usuario({
+    required this.id,
+    required this.nombre,
+    required this.edad,
+  });
 
 }
