8450575dd5f097f2c1f72be9684c126b0f1695f6
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-04 21:15:02 -0500

    update Usuario

--- lib/models/usuario.dart
+++ lib/models/usuario.dart
@@ -1,17 +1,72 @@
-
+import  'dart:io';    // stdin.readLineSync para leer
+import 'package:uuid/uuid.dart'; // para generar id unico
 
 abstract class Usuario {
   String id;
   String nombre;
-  int edad;
+  DateTime fechaNacimiento;
+  String? fotoPerfil;
 
   Usuario({
-    required this.id,
     required this.nombre,
-    required this.edad,
-  });
+    required this.fechaNacimiento,
+    required this.fotoPerfil,
+  }): id = Uuid().v4(); // Generar un ID único al crear un usuario
 
-  void mostrarinfo(){
+  void obtenerEdad () {
+    print("Ingrese su fecha de nacimiento en formato yyyy-mm-dd: ");
+    String? leerNacimiento = stdin.readLineSync();
 
+    try {
+    
+    fechaNacimiento = DateTime.parse(leerNacimiento!); // convertir String a fecha
+    print("Fecha de nacimiento registrada!");
+      DateTime hoy = DateTime.now();
+      
+      int edad = hoy.year - fechaNacimiento.year; // calcular edad
+      if (edad < 0) {
+        print("La fecha de nacimiento ingresada es futura. Por favor, ingrese una fecha válida.");
+        return;
+      } else if (edad > 150) {
+        print("La edad calculada es mayor a 150 años. Por favor, ingrese una fecha válida.");
+        return;
+      } 
+
+    } catch (e) {
+
+      print("Ingrese un formato valido");
+      return;
+      }
+    }
+
+    void cambiarNombre () {
+    print("Ingrese su nuevo nombre: ");
+    String? leerNombre = stdin.readLineSync();
+    if (leerNombre != null && leerNombre.isNotEmpty) {
+      nombre = leerNombre;
+      print("Nombre actualizado a: $nombre");
+    } else {
+      print("Nombre no puede estar vacío.");
+    }
   }
+
+  void cambiarFotoPerfil () {
+    print("Ingrese la ruta de su nueva foto de perfil: ");
+    String? leerFoto = stdin.readLineSync();
+    if (leerFoto != null && leerFoto.isNotEmpty) {
+      fotoPerfil = leerFoto;
+      print("Foto de perfil actualizada a: $fotoPerfil");
+    } else {
+      print("Ruta de foto no puede estar vacía.");
+    }
+  }
+
+  void mostrarInformacion() {
+    print("ID: $id");
+    print("Nombre: $nombre");
+    print("Fecha de Nacimiento: ${fechaNacimiento.toLocal()}");
+    print("Foto de Perfil: ${fotoPerfil ?? 'No registrada'}");
+  }
+
+
 }
