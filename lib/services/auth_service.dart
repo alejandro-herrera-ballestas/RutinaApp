cc01c61f5a6bb1016278aa64bcde4edd9f5ea3f7
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-13 18:42:18 -0500

    Update auth_service.dart

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,15 @@
+class AuthService {
+
+  bool iniciarSesion(String correo, String contrasena) {
+    return true;
+  }
+
+  void cerrarSesion() {}
+
+  bool registrarUsuario() {
+    return true;
+  }
+
+  void recuperarContrasena(String correo) {}
+
+}
