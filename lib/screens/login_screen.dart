9bf0a9aae84db9cd3f44f9486e3f1d2ea68978ec
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-14 16:00:40 -0500

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,6 +4,8 @@ class LoginScreen extends StatefulWidget{
   const LoginScreen({super.key});
   @override
   State<LoginScreen> createState() => _LoginScreenState();
+  final TextEditingController UsuarioController = TextEditingController();
+  final TextEditingController con
 }
 
 class _LoginScreenState extends State<LoginScreen>  {
@@ -11,9 +13,21 @@ class _LoginScreenState extends State<LoginScreen>  {
   Widget build(BuildContext context)  {
     return  Scaffold(
       backgroundColor: const Color(0xFFFFFBF5),
+      appBar: AppBar(
+        title: const Text("Rutina App"),
+      ),
       body: Center(
-        child: Text("INICIAR SESION",
-        style: TextStyle(fontSize: 40,color: Colors.black87),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              "Login",
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.black87,
+              ),
+            ),
+          ],
         ),
       ),
     );
