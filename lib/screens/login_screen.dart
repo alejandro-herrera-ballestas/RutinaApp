1e130d8fb964d8ab33efcad5f7acd2b91ae66882
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-15 12:51:04 -0500

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,57 +3,145 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatefulWidget{
   const LoginScreen({super.key});
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();  // crear el state de login
 }
 
 class _LoginScreenState extends State<LoginScreen>  {
 
-  final TextEditingController usuarioController = TextEditingController();
+  final TextEditingController usuarioController = TextEditingController();    // controladores para guardar los datos
   final TextEditingController contrasenaController = TextEditingController();
+  bool ocultarContrasena = true;
 
   @override
   Widget build(BuildContext context)  {
     return  Scaffold(
-      backgroundColor: const Color(0xFFFFFBF5),
+      backgroundColor: const Color(0xFFDCD3D1), // color de fondo
       appBar: AppBar(
-        title: const Text("Rutina App"),
+        backgroundColor: const Color(0xFFFFFBF5),   // barra
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          "Rutina App",
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+            color: Colors.black87,
+            letterSpacing: 1.2,
+          ),
+        ),
       ),
+
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
+            Icon(
+              Icons.calendar_month,
+              size: 90,
+            ),
             const Text(
               "Login",
               style: TextStyle(
                 fontSize: 40,
                 color: Colors.black87,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.normal,
               ),
             ),
+
             SizedBox(
-              height: 20,
-              width: 300,
+              height: 60,
             ),
-            TextFormField(
-              controller: usuarioController,
-              decoration: InputDecoration(
-                labelText: "Usuario",
-                hintText: "Ingrese su usuario",
-                prefixIcon: Icon(Icons.person),
+
+            SizedBox(     // espacio para poner el usuario
+              width: 300,
+              child: TextFormField(
+                controller: usuarioController,
+                decoration: const InputDecoration(
+                  labelText: "Usuario",
+                  hintText: "Ingrese su usuario",
+                  prefixIcon: Icon(Icons.person),
+                ),
               ),
             ),
+
             SizedBox(
-              height: 20,
-              width: 300,
+                height: 20
             ),
-            TextFormField(
-              controller: contrasenaController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Contraseña",
-                hintText: "Ingrese su contraseña",
-                prefixIcon: Icon(Icons.password),
+
+             SizedBox(    // espacio para poner la contrasena
+              width: 300,
+                child: TextFormField(
+
+                  controller: contrasenaController,
+                  obscureText: ocultarContrasena,
+                  decoration:  InputDecoration(
+                    labelText: "Contraseña",
+                    hintText: "Ingrese su contraseña",
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          ocultarContrasena = !ocultarContrasena;
+                        });
+                      },
+                      icon: Icon(
+                        ocultarContrasena
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                    ),
+                  ),
+                )
+            ),
+
+            SizedBox(
+              height: 30,
+            ),
+            
+            SizedBox(
+              width: 300,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  final usuario = usuarioController.text;
+                  final contrasena = contrasenaController.text;
+                  if (usuario.isEmpty || contrasena.isEmpty) {
+                    // mostrar mensaje
+                    return;
+                  }
+                  print("Usuario: $usuario, Contraseña: $contrasena");
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF6D8B74),
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: const Text(
+                  "Iniciar sesión",
+                  style: TextStyle(fontSize: 18),
+                ),
               ),
-            )
+            ),
+
+            const SizedBox(height: 15),
+
+// BOTÓN REGISTRARSE
+            TextButton(
+              onPressed: () {
+                // pantalla de registro
+                // Navigator.push(context, MaterialPageRoute(builder: (_) => RegisterScreen()));
+              },
+              child: const Text(
+                "¿No tienes cuenta? Regístrate",
+                style: TextStyle(
+                  color: Colors.black87,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
           ],
         ),
       ),
