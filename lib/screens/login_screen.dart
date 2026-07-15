f6a150b789c76d2379763393d41b994ddd199613
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-15 11:28:50 -0500

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,11 +4,13 @@ class LoginScreen extends StatefulWidget{
   const LoginScreen({super.key});
   @override
   State<LoginScreen> createState() => _LoginScreenState();
-  final TextEditingController UsuarioController = TextEditingController();
-  final TextEditingController con
 }
 
 class _LoginScreenState extends State<LoginScreen>  {
+
+  final TextEditingController usuarioController = TextEditingController();
+  final TextEditingController contrasenaController = TextEditingController();
+
   @override
   Widget build(BuildContext context)  {
     return  Scaffold(
@@ -27,10 +29,44 @@ class _LoginScreenState extends State<LoginScreen>  {
                 color: Colors.black87,
               ),
             ),
+            SizedBox(
+              height: 20,
+              width: 300,
+            ),
+            TextFormField(
+              controller: usuarioController,
+              decoration: InputDecoration(
+                labelText: "Usuario",
+                hintText: "Ingrese su usuario",
+                prefixIcon: Icon(Icons.person),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+              width: 300,
+            ),
+            TextFormField(
+              controller: contrasenaController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Contraseña",
+                hintText: "Ingrese su contraseña",
+                prefixIcon: Icon(Icons.password),
+              ),
+            )
           ],
         ),
       ),
     );
   }
+
+  @override
+  void dispose(){
+    usuarioController.dispose();
+    contrasenaController.dispose();
+    super.dispose();
+  }
 }
+
+
 
