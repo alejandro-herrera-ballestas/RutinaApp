76c5721219d52119ef9aa6d971416bb5487579f0
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-18 18:00:28 -0500

    update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,65 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class HomeScreen extends StatefulWidget{
-
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
   @override
-  State<HomeScreen> createState() => _HomeScreenState();  // crear el state de Home
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  
+  @override
+  Widget build(BuildContext context) {
+    final String fechaHoy = DateFormat("d 'de' MMMM", 'es_ES').format(DateTime.now());
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFF8F5F2),
+
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFFFFBF5),
+        elevation: 0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        title: const Text(
+          "RutinaApp",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.black87,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications_none),
+            onPressed: () {},
+          ),
+        ],
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Hoy",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              fechaHoy,
+              style: const TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
