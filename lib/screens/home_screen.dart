0bfce79295945db11185804f022b120374af1afb
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-18 17:41:18 -0500

    Create home_screen.dart

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatefulWidget{
+
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();  // crear el state de Home
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  
+}
