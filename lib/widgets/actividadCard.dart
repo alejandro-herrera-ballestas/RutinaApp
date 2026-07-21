ea41239d324878883286044f077b13c64657dccd
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-21 18:11:38 -0500

    Update actividadCard.dart

---
+++ lib/widgets/actividadCard.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:rutina_app/models/actividad.dart';
+
+class ActividadCard extends StatelessWidget {
+
+  final Actividad actividad;
+
+  const ActividadCard({
+    super.key,
+    required this.actividad,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(    
+      child: ListTile(
+        leading: Image.asset(
+          actividad.rutaIMG,
+          width: 50,
+          height: 50,
+          fit: BoxFit.cover,
+        ),
+
+        title: Text(actividad.nombre),
+
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Hora: ${actividad.hora.format(context)}"),
+          ],
+        ),
+
+        trailing: const Icon(Icons.arrow_forward_ios),
+
+        onTap: () {
+          // Ir a la pantalla de detalle
+        },
+      ),
+    );
+  }
+}
