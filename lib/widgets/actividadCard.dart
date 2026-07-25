8b694452190b30d87960a148dbd1ec9cb6653dcc
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-25 17:08:41 -0500

    Update actividadCard.dart

--- lib/widgets/actividadCard.dart
+++ lib/widgets/actividadCard.dart
@@ -2,15 +2,15 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:rutina_app/models/actividad.dart';
-import 'package:rutina_app/screens/detalle_actividad_screen.dart';
 
 class ActividadCard extends StatelessWidget {
-
   final Actividad actividad;
+  final VoidCallback onTap;
 
   const ActividadCard({
     super.key,
     required this.actividad,
+    required this.onTap,
   });
 
   @override
@@ -18,24 +18,21 @@ class ActividadCard extends StatelessWidget {
     return Card(
       child: ListTile(
         leading: Image.file(
-            File(actividad.rutaIMG)
+          File(actividad.rutaIMG),
+          width: 60,
+          height: 60,
+          fit: BoxFit.cover,
         ),
 
         title: Text(actividad.nombre),
+
         subtitle: Text(
           actividad.hora.format(context),
         ),
 
         trailing: const Icon(Icons.arrow_forward_ios),
-        onTap: () {
-          Navigator.push(
-              context, 
-              MaterialPageRoute(builder: (context) => DetalleActividadScreen(
-                  actividad: actividad,
-                ),
-              ),
-          );
-        },
+
+        onTap: onTap,
       ),
     );
   }
