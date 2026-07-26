680040eb6f9c9c81c93dacbd14b7f831e56d6b49
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-26 17:02:43 -0500

    Update global.dart

--- lib/utils/global.dart
+++ lib/utils/global.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:rutina_app/services/auth_service.dart';
 import 'package:rutina_app/services/actividad_service.dart';
 import 'package:rutina_app/models/horario.dart';
@@ -5,3 +6,4 @@ import 'package:rutina_app/models/horario.dart';
 final ActividadService actividadService = ActividadService();
 final AuthService authService = AuthService();
 final Horario horarioDelDia = Horario(bloques: []);
+final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
