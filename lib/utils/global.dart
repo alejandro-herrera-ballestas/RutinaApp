7c9ad93544461966b1c95683f8a6ef62fb644bed
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-26 16:45:26 -0500

    update global

--- lib/utils/global.dart
+++ lib/utils/global.dart
@@ -1,5 +1,7 @@
 import 'package:rutina_app/services/auth_service.dart';
 import 'package:rutina_app/services/actividad_service.dart';
+import 'package:rutina_app/models/horario.dart';
 
 final ActividadService actividadService = ActividadService();
 final AuthService authService = AuthService();
+final Horario horarioDelDia = Horario(bloques: []);
