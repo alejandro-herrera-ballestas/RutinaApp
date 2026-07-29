01fe70bd49faf041c903255eba183ce2cc3716c0
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-29 14:10:46 -0500

    Update perfil_screen.dart

--- lib/screens/perfil_screen.dart
+++ lib/screens/perfil_screen.dart
@@ -1,3 +1,17 @@
-class PerfilScreen {
+import 'package:flutter/material.dart';
 
+class PerfilScreen extends StatefulWidget  {
+  const PerfilScreen ({super.key});
+
+  @override
+  State<PerfilScreen> createState() => _PerfilScreen();
+}
+
+class _PerfilScreen extends State<PerfilScreen> {
+  @override
+  Widget build(BuildContext context)  {
+    return Scaffold(
+
+    );
+  }
 }
