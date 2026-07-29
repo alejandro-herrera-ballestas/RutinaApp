16be17bd6d349d29ec24c302718df51d73f33eeb
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-29 14:51:25 -0500

    Update main_navigator_screen.dart

--- lib/screens/main_navigator_screen.dart
+++ lib/screens/main_navigator_screen.dart
@@ -1,18 +1,51 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:rutina_app/screens/home_screen.dart';
+import 'package:rutina_app/screens/calendario_screen.dart';
+import 'package:rutina_app/screens/perfil_screen.dart';
 
-class mainNavigatorScreen extends StatefulWidget  {
-  const mainNavigatorScreen ({super.key});
+class MainNavigatorScreen extends StatefulWidget  {
+  const MainNavigatorScreen ({super.key});
 
   @override
-  State<mainNavigatorScreen> createState() => _mainNavigatorScreen();
+  State<MainNavigatorScreen> createState() => _MainNavigatorScreen();
 }
 
-class _mainNavigatorScreen extends State<mainNavigatorScreen> {
+class _MainNavigatorScreen extends State<MainNavigatorScreen> {
+
+  int _indiceActual = 0;  // guardar cual pestaña esta seleccionada
+  // lista de pantallas
+  final List<Widget> _pantallas = [const HomeScreen(), const CalendarioScreen(), const PerfilScreen()];
+
   @override
   Widget build(BuildContext context)  {
     return Scaffold(
-      
+      body: _pantallas[_indiceActual],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _indiceActual,
+        onTap: (indice)  {
+            setState(() {
+              _indiceActual = indice;
+            });
+        },
+
+        // Iconos
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Inicio",
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: "Calendario",
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Perfil",
+          ),
+        ],
+      ),
     );
   }
 }
