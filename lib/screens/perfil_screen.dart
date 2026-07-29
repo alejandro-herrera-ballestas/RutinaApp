0c7a23a406de4d9ddbff6b5ce802265011f60fae
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-29 15:16:39 -0500

    Update perfil_screen.dart

--- lib/screens/perfil_screen.dart
+++ lib/screens/perfil_screen.dart
@@ -11,7 +11,36 @@ class _PerfilScreen extends State<PerfilScreen> {
   @override
   Widget build(BuildContext context)  {
     return Scaffold(
+    backgroundColor: const Color(0xFFF8F5F2), // color de fondo
+    appBar: AppBar(
+      backgroundColor: const Color(0xFFFFFBF5),   // color del appBar
+      elevation: 0,
+      centerTitle: true,
+      title: const Text(
+      "Perfil",
+      style: TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+      color: Colors.black87,
+        ),
+      ),
+    actions: [
+      IconButton(
+        icon: const Icon(Icons.notifications_none),   // icono notificacion
+        onPressed: () {},
+         ),
+        ],
+      ),
 
+      body: Padding(
+          padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+          ],
+        ),
+      ),
     );
   }
 }
