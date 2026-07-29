45060c509f53910ce462e41df4d68d888451b3ca
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-29 14:08:11 -0500

    Create main_navigator_screen.dart

---
+++ lib/screens/main_navigator_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class mainNavigatorScreen extends StatefulWidget  {
+  const mainNavigatorScreen ({super.key});
+
+  @override
+  State<mainNavigatorScreen> createState() => _mainNavigatorScreen();
+}
+
+class _mainNavigatorScreen extends State<mainNavigatorScreen> {
+  @override
+  Widget build(BuildContext context)  {
+    return Scaffold(
+      
+    );
+  }
+}
