88525b81d9257025017380c29db17988a58bab8c
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-07-31 14:00:32 -0500

    Update global.dart

--- lib/utils/global.dart
+++ lib/utils/global.dart
@@ -7,3 +7,11 @@ final ActividadService actividadService = ActividadService();
 final AuthService authService = AuthService();
 final Horario horarioDelDia = Horario(bloques: []);
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
+
+final total = actividadService.cantidadActividades();
+final completadas = actividadService.actividadesCompletadas();
+final pendientes = total - completadas;
+
+final porcentaje = total == 0
+    ? 0
+    : ((completadas / total) * 100).round();
