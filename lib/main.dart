67b44ecf8b25a082c182f9860d47eda42612ede4
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-08-01 18:55:52 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:rutina_app/screens/login_screen.dart';
-import 'package:rutina_app/screens/main_navigator_screen.dart';
 import 'package:rutina_app/utils/global.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://nvevfwmpbgbbjfwlyabe.supabase.co',
+    anonKey: 'sb_publishable_8Fz72K1oNMtI-O2g9HcXuA_dna3UmzV',
+  );
+
   await initializeDateFormatting('es_ES', null);
+
   runApp(const MyApp());
 }
 
