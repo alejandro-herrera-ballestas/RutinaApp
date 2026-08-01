bf55db2cdd4026354033e22588f8c31a34ac1ea3
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-08-01 18:58:45 -0500

    Update global.dart

--- lib/utils/global.dart
+++ lib/utils/global.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:rutina_app/models/horario.dart';
 import 'package:rutina_app/services/auth_service.dart';
 import 'package:rutina_app/services/actividad_service.dart';
-import 'package:rutina_app/models/horario.dart';
 
 final ActividadService actividadService = ActividadService();
 final AuthService authService = AuthService();
+
 final Horario horarioDelDia = Horario(bloques: []);
+
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
+
+final SupabaseClient supabase = Supabase.instance.client;
