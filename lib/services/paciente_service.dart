818b06b276a5baa6947ccd4c4128a6da16ba13d7
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-08-02 18:25:21 -0500

    Update paciente_service.dart

--- lib/services/paciente_service.dart
+++ lib/services/paciente_service.dart
@@ -1,5 +1,16 @@
-import 'package:rutina_app/utils/global.dart';
+
+import 'package:rutina_app/models/paciente.dart';
+import 'package:rutina_app/services/usuario_service.dart';
 
 class PacienteService {
 
+  final UsuarioService usuarioService = UsuarioService();
+
+  Future<void> crearPaciente(Paciente paciente) {
+    throw UnimplementedError();
+  }
+
+  Future<Paciente> obtenerPaciente(String id) {
+    throw UnimplementedError();
+  }
 }
