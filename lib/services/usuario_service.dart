dcc1f428d1811d86476d087b3f366e073fc2ed35
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-08-02 18:23:15 -0500

    Update usuario_service.dart

--- lib/services/usuario_service.dart
+++ lib/services/usuario_service.dart
@@ -1,3 +1,21 @@
+import 'package:rutina_app/models/usuario.dart';
+
 class UsuarioService {
-  
+
+  Future<String> crearUsuario(Usuario usuario) async {
+    throw UnimplementedError();
+  }
+
+  Future<Usuario?> obtenerUsuario(String id) async {
+    throw UnimplementedError();
+  }
+
+  Future<void> actualizarUsuario(Usuario usuario) async {
+    throw UnimplementedError();
+  }
+
+  Future<void> eliminarUsuario(String id) async {
+    throw UnimplementedError();
+  }
+
 }
