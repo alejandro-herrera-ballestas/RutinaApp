a8eda3034d4ae91708a4f51b1a0258fbe4d660fe
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-08-02 19:37:38 -0500

    Update paciente_service.dart

--- lib/services/paciente_service.dart
+++ lib/services/paciente_service.dart
@@ -1,17 +1,45 @@
 
 import 'package:rutina_app/models/paciente.dart';
 import 'package:rutina_app/services/usuario_service.dart';
+import 'package:rutina_app/utils/global.dart';
 
 class PacienteService {
 
   final UsuarioService usuarioService = UsuarioService();
 
-  Future<void> crearPaciente(Paciente paciente) async{
-    throw UnimplementedError();
+  Future<void> crearPaciente(Paciente paciente) async {
+    try {
+      final String idUsuario = await usuarioService.crearUsuario(paciente);
+
+      await supabase
+          .from('pacientes')
+          .insert({
+        'usuario_id': idUsuario,
+      });
+
+    } catch (e) {
+      throw Exception("Error al crear paciente: $e");
+    }
   }
 
   Future<Paciente> obtenerPaciente(String id) async{
-    throw UnimplementedError();
+    try {
+      final response = await supabase
+          .from('pacientes')
+          .select('''
+      id,
+      usuarios(
+        nombre,
+        fecha_nacimiento,
+        foto_perfil
+      )
+    ''')
+          .eq('id', id)
+          .single();
+
+    } catch (e) {
+      throw Exception('Error al obtener paciente: $e')
+    }
   }
 
   Future<List<Paciente>> obtenerTodosPacientes()  async{
