acc4924f7cb69b1c4b688ceddf65dfcf137fccff
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-08-12 15:50:40 -0500

    Update actividad.dart

--- lib/models/actividad.dart
+++ lib/models/actividad.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Actividad {
-
   String id;
   String nombre;
   String descripcion;
@@ -42,19 +41,52 @@ class Actividad {
     if (nuevoNombre != null && nuevoNombre.isNotEmpty) {
       nombre = nuevoNombre;
     }
+
     if (nuevaDescripcion != null &&
         nuevaDescripcion.isNotEmpty) {
       descripcion = nuevaDescripcion;
     }
+
     if (nuevaRutaIMG != null &&
         nuevaRutaIMG.isNotEmpty) {
       rutaIMG = nuevaRutaIMG;
     }
-    if (nuevaHora != null)  {
+
+    if (nuevaHora != null) {
       hora = nuevaHora;
     }
+
     if (nuevaDuracion != null) {
       duracion = nuevaDuracion;
     }
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'nombre': nombre,
+      'descripcion': descripcion,
+      'imagen': rutaIMG,
+      'hora':
+      '${hora.hour.toString().padLeft(2, '0')}:${hora.minute.toString().padLeft(2, '0')}',
+      'duracion': duracion.inSeconds,
+    };
+  }
+
+  factory Actividad.fromMap(Map<String, dynamic> map) {
+    final partesHora = map['hora'].toString().split(':');
+
+    return Actividad(
+      id: map['id'],
+      nombre: map['nombre'],
+      descripcion: map['descripcion'],
+      rutaIMG: map['imagen'] ?? '',
+      hora: TimeOfDay(
+        hour: int.parse(partesHora[0]),
+        minute: int.parse(partesHora[1]),
+      ),
+      duracion: Duration(
+        seconds: map['duracion'],
+      ),
+    );
   }
 }
