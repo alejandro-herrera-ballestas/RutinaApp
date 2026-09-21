500f5fa2e34d6bb27128a76d86a6706cc7e326bb
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-09-20 22:31:24 -0500

    Update perfil_screen.dart

--- lib/screens/perfil_screen.dart
+++ lib/screens/perfil_screen.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:rutina_app/screens/login_screen.dart';
 import 'package:rutina_app/screens/vincular_paciente_screen.dart';
-import 'package:rutina_app/models/paciente.dart';
-import 'package:rutina_app/models/cuidador.dart';
 import 'package:rutina_app/utils/global.dart';
 
 class PerfilScreen extends StatefulWidget {
