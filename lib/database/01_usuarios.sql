e082588054b1197dc8934da348e6843403f3bcd9
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-09-23 01:43:31 +0000

    delete database

--- lib/database/01_usuarios.sql
+++
@@ -1,7 +0,0 @@
-create table usuarios (
-    id uuid primary key default gen_random_uuid(),
-    nombre text not null,
-    fecha_nacimiento date,
-    foto_perfil text,
-    fecha_registro timestamp with time zone default now()
-);
--- lib/database/02_pacientes.sql
+++
@@ -1,9 +0,0 @@
-create table pacientes (
-  id uuid primary key default gen_random_uuid(),
-  usuario_id uuid not null unique,
-
-  constraint fk_usuario
-    foreign key (usuario_id)
-    references usuarios(id)
-    on delete cascade
-);
